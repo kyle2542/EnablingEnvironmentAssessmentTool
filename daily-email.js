16bee7e7a523f550ad48419970fe3e26709fabd3
yourboykyle <kyle.lyon2542@gmail.com>
2024-03-20 13:49:32 -0400

    Added daily email

---
+++ daily-email.js
@@ -0,0 +1,24 @@
+const nodemailer = require('nodemailer');
+
+const transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+        user: 'kyle.lyon2542@gmail.com',
+        pass: 'mjzr xoji ndpc etxn'
+    }
+});
+
+const mailOptions = {
+    from: 'kyle.lyon2542@gmail.com',
+    to: 'klyon2@ocdsb.ca',
+    subject: 'Test Daily Email',
+    text: 'Test daily email being sent for use with SageTeaEdge.'
+};
+
+transporter.sendMail(mailOptions, (error, info) => {
+    if (error) {
+        console.error('Error occurred:', error);
+    } else {
+        console.log('Email sent:', info.response);
+    }
+});
