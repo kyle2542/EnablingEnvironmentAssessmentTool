a23c8f300419575b4a5b01dd4e740fbf1d753de8
yourboykyle <kyle.lyon2542@gmail.com>
2024-03-22 08:15:38 -0400

    Added emailing to building and testing binaries

--- index.js
+++ index.js
@@ -1,12 +1,43 @@
-console.log("Hello world!");
+const nodemailer = require('nodemailer');
 
-let currentDate = new Date();
+let projectName = "SageTeaEdge";
+let binaryPath = "dist";
+let binary1 = "index-linux";
+let binary2 = "index-macos";
+let binary3 = "index-win.exe";
+let emailSubject = "SageTea Github Build Report";
+let buildReport = `Build Report for ${projectName}:\n\n`;
 
-let year = currentDate.getFullYear();
-let month = currentDate.getMonth() + 1;
-let day = currentDate.getDate();
-let hours = currentDate.getHours();
-let minutes = currentDate.getMinutes();
-let seconds = currentDate.getSeconds();
+try {
+    buildReport += `Built Binaries:\n${binaryPath}/${binary1}\n${binaryPath}/${binary2}\n${binaryPath}/${binary3}`;
 
-console.log(`${year}-${month}-${day} ${hours}:${minutes}:${seconds}`);
+    buildReport += `Test for ${binary1} succeeded!`;
+} catch (e) {
+    buildReport += `Test for ${binary1} failed!\n\nDetails of the error can be found below:\n\n${e}`;
+}
+
+console.log(buildReport);
+
+// Send build report in an email
+const transporter = nodemailer.createTransport({
+    service: 'gmail',
+    auth: {
+        user: 'testingemail2542@gmail.com',
+        pass: 'modd hlci zpkq unve'
+    }
+});
+
+const mailOptions = {
+    from: 'testingemail2542@gmail.com',
+    to: 'support@sagetea.ai',
+    subject: emailSubject,
+    text: buildReport
+};
+
+transporter.sendMail(mailOptions, (error, info) => {
+    if (error) {
+        console.error('Error occurred:', error);
+    } else {
+        console.log('Email sent:', info.response);
+    }
+});
