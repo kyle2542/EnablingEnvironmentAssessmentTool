aed17e963fa1bb44dc7cc7cc9a8d47f12c591ebb
yourboykyle <kyle.lyon2542@gmail.com>
2024-04-08 18:30:59 -0400

    Added .deb file testing

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 (async () => {
 
-let axios = require("axios")
+let axios = require("axios");
+let fs = require("fs");
+let exec = require("child_process");
 let nodemailer = require('nodemailer');
 
 let githubUsername = 'kyle2542';
@@ -14,6 +16,30 @@ let buildReport = `Build Report for ${projectName}:\n\n`;
 
 try {
     buildReport += `Built Binaries:\n${binaryPath}/${binary1}\n${binaryPath}/${binary2}\n${binaryPath}/${binary3}\n\n`;
+
+    // Download .deb file
+    const debUrl = "https://repo.sagetea.ai/repo/amd64/sageteacloud64-4.364.amd64.deb";
+    const debFileName = "sageteacloud64-4.364.amd64.deb"; // Specify the name of the downloaded file
+    const response = await axios.get(debUrl, { responseType: 'stream' });
+    const writer = fs.createWriteStream(debFileName);
+    response.data.pipe(writer);
+    await new Promise((resolve, reject) => {
+        writer.on('finish', resolve);
+        writer.on('error', reject);
+    });
+
+    // Run the .deb file
+    exec.exec(`sudo dpkg -i ${debFileName}`, async (error, stdout, stderr) => {
+        if (error) {
+            buildReport += `Error running .deb file: ${error.message}\n\n`;
+            return;
+        }
+        if (stderr) {
+            buildReport += `stderr output: ${stderr}\n\n`;
+            return;
+        }
+        buildReport += `stdout output: ${stdout}\n\n`;
+    });
 
     buildReport += `Test for ${binary1} succeeded!\n\n\n\n`;
 } catch (e) {
@@ -68,7 +94,3 @@ transporter.sendMail(mailOptions, (error, info) => {
 });
 
 })();
-
-// Build report only show this repository
-
-// C program
